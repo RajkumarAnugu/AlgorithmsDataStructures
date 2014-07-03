fbe9ddc40baf25c11ed829d58ce996288409ec37
Guo Junjun <guojunjun@gmail.com>
2014-07-03 19:31:50 +0200

    merge sort

---
+++ sorting/MergeSort.java
@@ -0,0 +1,67 @@
+package sorting;
+
+public class MergeSort {
+    
+    public static void sort(int [] a){
+        if (a.length <= 1) {
+            return;
+        }
+        int[] first = new int[a.length / 2];
+        int[] second = new int[a.length - first.length];
+        for (int i = 0; i < first.length; i++) {
+            first[i] = a[i];
+        }
+        for (int i = 0; i < second.length; i++) {
+            second[i] = a[first.length + i];
+        }
+        System.out.println("first : " + MergeSort.toString(first));
+        sort(first);
+        System.out.println("second: " + MergeSort.toString(second));
+        sort(second);
+        System.out.println("merge!");
+        merge(first, second, a);
+        System.out.println("Merge: ");
+        System.out.println("    first : " + MergeSort.toString(first));
+        System.out.println("    second: " + MergeSort.toString(second));
+        System.out.println("    a     : " + MergeSort.toString(a));
+    }
+    
+    private static void merge(int[] first, int[] second, int[] a) {
+        int iFirst = 0, iSecond = 0, j = 0;
+        while (iFirst < first.length && iSecond < second.length) {
+            if (first[iFirst] < second[iSecond]) {
+                a[j] = first[iFirst];
+                iFirst++;
+            } else {
+                a[j] = second[iSecond];
+                iSecond++;
+            }
+            j++;
+        }
+        while (iFirst < first.length) {
+            a[j] = first[iFirst];
+            iFirst++;
+            j++;
+        }
+        while (iSecond < second.length) {
+            a[j] = second[iSecond];
+            iSecond++;
+            j++;
+        }
+    }
+    
+    public static String toString(int[] a) {
+        String s = "";
+        for (int i = 0; i < a.length; i++) {
+            s += a[i] + ", ";
+        }
+        return s;
+        }
+    
+    
+    public static void main(String[] args) {
+        int[] a = { 9, 8, 7, 6, 5, 4, 3, 2, 1, 10, 11 };
+        MergeSort.sort(a);
+        System.out.println(MergeSort.toString(a));
+    }
+}
